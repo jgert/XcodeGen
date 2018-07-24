6f4c35fd3f95a0a8bedaa9ebe0c943d327bdd022
Jakub Gert <kubagert@gmail.com>
2018-07-24 18:21:29 +0200

    Added basic NSRegularExpression for filter out sources

--- Sources/ProjectSpec/TargetSource.swift
+++ Sources/ProjectSpec/TargetSource.swift
@@ -10,6 +10,7 @@ public struct TargetSource: Equatable {
     public var name: String?
     public var compilerFlags: [String]
     public var excludes: [String]
+    public var excludePatterns: [NSRegularExpression]
     public var type: SourceType?
     public var optional: Bool
     public var buildPhase: BuildPhase?
@@ -123,6 +124,7 @@ public struct TargetSource: Equatable {
         name: String? = nil,
         compilerFlags: [String] = [],
         excludes: [String] = [],
+        excludePatterns: [NSRegularExpression] = [],
         type: SourceType? = nil,
         optional: Bool = optionalDefault,
         buildPhase: BuildPhase? = nil,
@@ -133,6 +135,7 @@ public struct TargetSource: Equatable {
         self.name = name
         self.compilerFlags = compilerFlags
         self.excludes = excludes
+        self.excludePatterns = excludePatterns
         self.type = type
         self.optional = optional
         self.buildPhase = buildPhase
@@ -169,6 +172,10 @@ extension TargetSource: JSONObjectConvertible {
 
         headerVisibility = jsonDictionary.json(atKeyPath: "headerVisibility")
         excludes = jsonDictionary.json(atKeyPath: "excludes") ?? []
+        let regexPatterns: [String] = jsonDictionary.json(atKeyPath: "excludePatterns") ?? []
+        excludePatterns = try regexPatterns.map({
+            try NSRegularExpression(pattern: $0)
+        })
         type = jsonDictionary.json(atKeyPath: "type")
         optional = jsonDictionary.json(atKeyPath: "optional") ?? TargetSource.optionalDefault
 
--- Sources/XcodeGenKit/SourceGenerator.swift
+++ Sources/XcodeGenKit/SourceGenerator.swift
@@ -21,6 +21,7 @@ class SourceGenerator {
     let pbxProj: PBXProj
 
     var targetSourceExcludePaths: Set<Path> = []
+    var excludePatterns: [NSRegularExpression] = []
     var defaultExcludedFiles = [
         ".DS_Store",
     ]
@@ -308,10 +309,23 @@ class SourceGenerator {
         )
     }
 
+    func isExcludedPattern(_ path: Path) -> Bool {
+        return excludePatterns.reduce(false) {
+            (result: Bool, expression: NSRegularExpression) -> Bool in
+
+            let string: String = path.string
+            let range = NSRange(location: 0, length: string.count)
+            let matches = expression.matches(in: string, range: range)
+
+            return result || (matches.count > 0)
+         }
+    }
+
     /// Checks whether the path is not in any default or TargetSource excludes
     func isIncludedPath(_ path: Path) -> Bool {
         return !defaultExcludedFiles.contains(where: { path.lastComponent.contains($0) })
             && !targetSourceExcludePaths.contains(path)
+            && !isExcludedPattern(path)
     }
 
     /// Gets all the children paths that aren't excluded
@@ -465,6 +479,7 @@ class SourceGenerator {
 
         // generate excluded paths
         targetSourceExcludePaths = getSourceExcludes(targetSource: targetSource)
+        excludePatterns = targetSource.excludePatterns
 
         let type = targetSource.type ?? (path.isFile || path.extension != nil ? .file : .group)
         let createIntermediateGroups = targetSource.createIntermediateGroups ?? project.options.createIntermediateGroups
